6caeeed9fd4cbec7ad9b530bb9091937d210518d
Connor Collins <73761251+Poliphilus@users.noreply.github.com>
2022-10-24 14:36:09 +0200

    Lava Coffee site upload

---
+++ work-probes-2022/lava-coffee/script.js
@@ -0,0 +1,46 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
+}
+
+// code above is for slide show
+
+
+
+
+// header sticky
+
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("myHeader");
+
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its 
+// scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
